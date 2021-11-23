8b081d18b5bbe5b465ee8a1610e7ad4fdcc373b0
jambottle <jaewon.walks@gmail.com>
2021-11-24 06:10:33 +0900

    Initial commit

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>Dashboard Page</div>
+</template>
+
+<script>
+export default {
+  name: 'Dashboard',
+
+  data() {
+    return {};
+  },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/GridSystem.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>Grid System Page</div>
+</template>
+
+<script>
+export default {
+  name: 'GridSystem',
+
+  data() {
+    return {};
+  },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
